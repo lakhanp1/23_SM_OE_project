e8edda2ca13f8913f2f89a6d6e083a09b43108b8
Lakhansing Pardeshi <lakhanp1@gmail.com>
2019-07-29 16:07:12 +0800

    added bigWig matrix generation for peak summit regions

--- scripts/01_preprocess_TF.R
+++ scripts/01_preprocess_TF.R
@@ -1,8 +1,6 @@
 library(chipmine)
 library(org.Anidulans.eg.db)
 library(TxDb.Anidulans.AspGD.GFF)
-library(foreach)
-library(doParallel)
 library(here)
 
 
@@ -31,8 +29,12 @@ other_dataPath <- here::here("data", "other_data")
 file_tf_macs2 <- paste(TF_dataPath, "/", "sample_tf_macs2.list", sep = "")
 file_tf <- paste(TF_dataPath, "/", "sample_tf.list", sep = "")
 
-matrixType <- "normalizedmatrix"
-matrixDim = c(200, 200, 100, 10)
+matrixType <- "2kb_summit"
+up <- 2000
+down <- 2000
+body <- 0
+bin <- 10
+matrixDim = c(c(up, body, down)/bin, bin)
 
 geneSet <- data.table::fread(file = file_genes, header = F,
                              col.names = c("chr", "start", "end", "geneId", "score", "strand")) %>%
@@ -59,38 +61,59 @@ tfInfo <- get_sample_information(
   profileMatrixSuffix = matrixType)
 
 
-i <- 1
+i <- 25
 
-## annotate peaks and prepare gene level annotation file
-for(i in 1:nrow(tfInfo)){
+for(i in 24:nrow(tfInfo)){
   
+  ## annotate peaks and prepare gene level annotation file
   peakType <- dplyr::case_when(
     tfInfo$peakType[i] == "narrow" ~ "narrowPeak",
     tfInfo$peakType[i] == "broad" ~ "broadPeak"
   )
   
-  peakAn <- narrowPeak_annotate(
-    peakFile = tfInfo$peakFile[i],
-    txdb = txDb,
-    fileFormat = peakType,
-    includeFractionCut = 0.7,
-    bindingInGene = FALSE,
-    promoterLength = 500,
-    insideSkewToEndCut = 0.7,
-    reportPseudo = FALSE,
-    output = tfInfo$peakAnno[i])
+  # peakAn <- narrowPeak_annotate(
+  #   peakFile = tfInfo$peakFile[i],
+  #   txdb = txDb,
+  #   fileFormat = peakType,
+  #   includeFractionCut = 0.7,
+  #   bindingInGene = FALSE,
+  #   promoterLength = 500,
+  #   insideSkewToEndCut = 0.7,
+  #   reportPseudo = FALSE,
+  #   output = tfInfo$peakAnno[i])
+  # 
+  # if( !is.null(peakAn) ){
+  #   tfDf <- gene_level_peak_annotation(
+  #     sampleId = tfInfo$sampleId[i],
+  #     peakAnnotation = tfInfo$peakAnno[i],
+  #     genesDf = geneSet,
+  #     peakFile = tfInfo$peakFile[i],
+  #     bwFile = tfInfo$bwFile[i],
+  #     outFile = tfInfo$peakTargetFile[i])
+  # }
   
-  if( !is.null(peakAn) ){
-    tfDf <- gene_level_peak_annotation(
-      sampleId = tfInfo$sampleId[i],
-      peakAnnotation = tfInfo$peakAnno[i],
-      genesDf = geneSet,
-      peakFile = tfInfo$peakFile[i],
-      bwFile = tfInfo$bwFile[i],
-      outFile = tfInfo$peakTargetFile[i],
-      bindingInGene = FALSE)
+  
+  ## create profile matrix of 2kb region around peak summit
+  peaksGr <- rtracklayer::import(con = tfInfo$peakFile[i], format = peakType)
+  
+  if(length(peaksGr) > 0){
+    if(peakType == "broadPeak"){
+      mcols(peaksGr)$peak <- round(width(peaksGr) / 2)
+    }
+    
+    peakSummitGr <- GenomicRanges::narrow(x = peaksGr,
+                                          start = pmax(peaksGr$peak, 1),
+                                          width = 1)
+    
+    profileMat <- bigwig_profile_matrix(bwFile = tfInfo$bwFile[i],
+                                        regions = peakSummitGr,
+                                        signalName = tfInfo$profileName[i],
+                                        extend = c(up, down),
+                                        targetName = "summit",
+                                        storeLocal = T,
+                                        localPath = tfInfo$matFile[i])
   }
-  
+
 }
 
 
