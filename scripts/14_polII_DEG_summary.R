26abb3f7f0dda94f9bd6cedfdeb975ce69205a32
Lakhansing Pardeshi <lakhanp1@gmail.com>
2020-06-22 17:06:22 +0800

    updated combined polII DEG heatmap

--- scripts/14_polII_DEG_summary.R
+++ scripts/14_polII_DEG_summary.R
@@ -25,6 +25,7 @@ if(!dir.exists(outDir)){
 diffDataPath <- here::here("analysis", "06_polII_diff")
 file_sampleInfo <- here::here("data", "reference_data", "polII_sample_info.txt")
 file_RNAseq_info <- here::here("data", "reference_data", "polII_DESeq2_DEG_info.txt")
+file_degIds <- here::here("data", "reference_data", "polII_DEG_ids.txt")
 
 useAllGroupsSamples <- FALSE
 
@@ -37,8 +38,10 @@ orgDb <- org.Anidulans.FGSCA4.eg.db
 col_geneId <- "GID"
 
 ###########################################################################
+degIds <- suppressMessages(readr::read_tsv(file = file_degIds))
 
-rnaseqInfo <- get_diff_info(degInfoFile = file_RNAseq_info, dataPath = diffDataPath) 
+rnaseqInfo <- get_diff_info(degInfoFile = file_RNAseq_info, dataPath = diffDataPath)
+
 
 rowId <- 1
 degData <- NULL
@@ -46,7 +49,8 @@ degData <- NULL
 for (rowId in 1:nrow(rnaseqInfo)) {
   
   tmpDf <- suppressMessages(readr::read_tsv(file = rnaseqInfo$deg[rowId])) %>% 
-    dplyr::select(geneId, contrast, log2FoldChange, padj, GENE_NAME)
+    dplyr::select(geneId, contrast, log2FoldChange, padj, GENE_NAME) %>% 
+    dplyr::mutate(comparison = rnaseqInfo$comparison[rowId])
   
   degData <- dplyr::bind_rows(degData, tmpDf)
 }
@@ -54,7 +58,7 @@ for (rowId in 1:nrow(rnaseqInfo)) {
 
 degData <- dplyr::mutate(
   degData,
-  contrast = stringr::str_replace(string = contrast, pattern = "condition_", replacement = ""),
+  # contrast = stringr::str_replace(string = contrast, pattern = "(condition_|SM_TF_)", replacement = ""),
   diff = dplyr::case_when(
     padj <= cutoff_fdr & log2FoldChange <= cutoff_down ~ "down",
     padj <= cutoff_fdr & log2FoldChange >= cutoff_up ~ "up",
@@ -63,7 +67,7 @@ degData <- dplyr::mutate(
 ) %>% 
   dplyr::filter(diff != "noDEG")
 
-degStats <- dplyr::group_by(degData, contrast, diff) %>% 
+degStats <- dplyr::group_by(degData, comparison, diff) %>% 
   dplyr::count() %>% 
   tidyr::pivot_wider(
     names_from = diff, values_from = n, values_fill = list(n = 0)
@@ -75,7 +79,7 @@ readr::write_tsv(x = degStats, path = paste(diffDataPath, "/polII_DEG.stats.tab"
 diffDf <- tidyr::pivot_wider(
   data = degData,
   id_cols = c(geneId),
-  names_from = contrast,
+  names_from = comparison,
   values_from = diff,
   values_fill = list(diff = "noDEG")
 )
@@ -88,7 +92,7 @@ readr::write_tsv(x = diffDf, path = paste(outPrefix, ".diff.tab", sep = ""))
 lfcDf <- tidyr::pivot_wider(
   data = degData,
   id_cols = c(geneId),
-  names_from = contrast,
+  names_from = comparison,
   values_from = log2FoldChange,
   values_fill = list(log2FoldChange = 0)
 )
@@ -99,41 +103,59 @@ lfcMat <- as.data.frame(lfcDf) %>%
   tibble::column_to_rownames(var = "geneId") %>% 
   as.matrix()
 
-colAnDf <- tibble::tibble(contrast = colnames(lfcMat)) %>% 
-  dplyr::left_join(y = degStats, by = "contrast") %>% 
+colAnDf <- tibble::tibble(comparison = colnames(lfcMat)) %>% 
+  dplyr::left_join(y = degStats, by = "comparison") %>% 
   dplyr::mutate(
     total = up + down,
+    copy = stringr::str_replace(
+      string = comparison, pattern = ".*_(\\w+Copy)_OE.*", replacement = "\\1"
+    ),
     fraction = label_percent(accuracy = 0.01)(total/nrow(lfcMat))
   ) %>% 
   as.data.frame() %>% 
-  tibble::column_to_rownames(var = "contrast")
+  tibble::column_to_rownames(var = "comparison")
 
 
 htAn <- ComplexHeatmap::HeatmapAnnotation(
+  copy = colAnDf$copy,
   up = anno_text(
-    x = colAnDf$up, which = "column", rot = 90, just = "center", location = 0.5,
-    gp = gpar(fill = "#b3ffb3", col = "black", border = "black"),
+    x = colAnDf$up, rot = 90, just = "center", location = 0.5,
+    gp = gpar(fill = "#ffb3c4", col = "black", border = "black"),
     height = max_text_width(text = colAnDf$up)*1.5
   ),
   down = anno_text(
-    x = colAnDf$down, which = "column", rot = 90, just = "center", location = 0.5,
-    gp = gpar(fill = "#ff9999", col = "black", border = "black"),
+    x = colAnDf$down, rot = 90, just = "center", location = 0.5,
+    gp = gpar(fill = "#d8daf3", col = "black", border = "black"),
     height = max_text_width(text = colAnDf$down)*1.5
   ),
   fraction = anno_text(
-    x = colAnDf$fraction, which = "column", rot = 90, just = "center", location = 0.5,
+    x = colAnDf$fraction, rot = 90, just = "center", location = 0.5,
     gp = gpar(col = "black", border = "black"),
     height = max_text_width(text = colAnDf$fraction)*1.2
   ),
-  # tmp = anno_simple(x = colAnDf$total/nrow(lfcMat)),
-  show_annotation_name = TRUE
+  which = "column",
+  col = list(
+    copy = c("sCopy" = "#ff7f00", "msCopy" = "#8dd3c7")
+  ),
+  annotation_label = c(
+    "copy number", "DEG Up", "DEG down", "fraction"
+  ),
+  show_annotation_name = TRUE,
+  annotation_name_side = "right",
+  annotation_legend_param = list(
+    copy = list(
+      title = "Copy number", ncol = 1, title_position = "leftcenter-rot",
+      title_gp = gpar(fontsize = 16, fontface = "bold"),
+      labels_gp = gpar(fontsize = 14)
+    )
+  )
 )
 
 
-ht <- ComplexHeatmap::Heatmap(
+ht_lfc <- ComplexHeatmap::Heatmap(
   matrix = lfcMat,
   name = "log2FoldChange",
-  col = colorRamp2(breaks = c(-3, 0, 3), colors = c("red", "black", "green")),
+  col = colorRamp2(breaks = c(-3, 0, 3), colors = c("#313695", "white", "#a50026")),
   top_annotation = htAn,
   row_title = "genes",
   column_title = "log2FoldChange heatmap of all significant DEGs in all OE/WT comparisons", 
@@ -144,15 +166,22 @@ ht <- ComplexHeatmap::Heatmap(
     string = colnames(lfcMat), pattern = "_vs_MH11036", replacement = ""
   ),
   heatmap_legend_param = list(
-    title = "log2(fold-change)", title_position = "leftcenter-rot",
-    legend_height = unit(4, "cm"),
-    title_gp = gpar(fontsize = 12, fontface = "bold")
+    title = "log2(fold-change)",  title_position = "leftcenter-rot",
+    legend_height = unit(4, "cm"), title_gp = gpar(fontsize = 16, fontface = "bold"),
+    labels_gp = gpar(fontsize = 14)
   ),
   use_raster = FALSE
 )
 
+htList <- ht_lfc
+
 png(filename = paste(outPrefix, ".lfc_heatmap.png", sep = ""), width = 6000, height = 4000, res = 400)
-draw(ht)
+draw(
+  htList,
+  merge_legends = TRUE,
+  legend_gap = unit(3, "cm"),
+  padding = unit(c(0.5, 0.5, 0.5, 1), "cm")
+)
 dev.off()
 
 ###########################################################################
