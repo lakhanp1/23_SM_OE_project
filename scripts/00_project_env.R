0d90f78735510ec7489578013485575ff8d71d17
Lakhansing Pardeshi <lakhanp1@gmail.com>
2021-03-01 17:28:07 +0800

    using renv for package management

---
+++ scripts/00_project_env.R
@@ -0,0 +1,26 @@
+suppressPackageStartupMessages(library(devtools))
+suppressPackageStartupMessages(library(topGO))
+suppressPackageStartupMessages(library(scales))
+suppressPackageStartupMessages(library(KEGGprofile))
+suppressPackageStartupMessages(library(clusterProfiler))
+suppressPackageStartupMessages(library(tm))  # for text mining
+suppressPackageStartupMessages(library(SnowballC)) # for text stemming
+suppressPackageStartupMessages(library(wordcloud)) # word-cloud generator
+suppressPackageStartupMessages(library(configr)) # word-cloud generator
+suppressPackageStartupMessages(library(renv))
+
+# initialize a new project-local environment with a private R library
+renv::init()
+
+# save the state of your project to renv.lock
+renv::snapshot()
+
+# list previous revisions of your lockfile
+renv::history()
+
+# restore the state of your project from renv.lock
+# renv::restore()
+
+
+# renv::deactivate()
+
