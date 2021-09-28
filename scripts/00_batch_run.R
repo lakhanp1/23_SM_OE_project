38849845740da010b06ad19dfacb05edfbc04270
Lakhansing Pardeshi <lakhanp1@gmail.com>
2021-09-28 23:33:18 +0800

    new blocks for batch run and renaming scripts

--- scripts/00_batch_run.R
+++ scripts/00_batch_run.R
@@ -1,88 +1,89 @@
 library(tidyverse)
 
-
 rm(list = ls())
+
 
 ###########################################################################
 #######################
 ### DESeq2 pairwise ###
 #######################
 
-## RNAseq DESeq2 differential gene expression batches
-file_RNAseq_info <- here::here("data", "reference_data", "polII_DESeq2_DEG_info.txt")
-diffDataPath <- here::here("analysis", "07_polII_diff")
-script_deseq2 <- here::here("scripts", "13_DESeq2_diff.R")
-
-runConfig <- suppressMessages(readr::read_tsv(file_RNAseq_info))
-
-i <- 1
-
-for (i in 1:nrow(runConfig)) {
-  cat("Processing DESeq2:", runConfig$comparison[i], "...\n")
-  
-  system2(
-    command = "Rscript",
-    args = c(script_deseq2, "--config", file_RNAseq_info, "--deg", runConfig$comparison[i]),
-    stdout = paste("logs/stdouterr.DESeq2.", runConfig$comparison[i],".log", sep = ""),
-    stderr = paste("logs/stdouterr.DESeq2.", runConfig$comparison[i],".log", sep = "")
-  )
-  
-}
+# ## RNAseq DESeq2 differential gene expression batches
+# file_RNAseq_info <- here::here("data", "reference_data", "polII_DESeq2_DEG_info.txt")
+# diffDataPath <- here::here("analysis", "06_polII_diff")
+# script_deseq2 <- here::here("scripts", "13_DESeq2_diff.R")
+# 
+# runConfig <- suppressMessages(readr::read_tsv(file_RNAseq_info))
+# 
+# rowId <- 45
+# 
+# for (rowId in 45:nrow(runConfig)) {
+#   cat("Processing DESeq2:", runConfig$comparison[rowId], "...\n")
+# 
+#   system2(
+#     command = "Rscript",
+#     args = c(script_deseq2, "--config", file_RNAseq_info, "--deg", runConfig$comparison[rowId]),
+#     stdout = paste("logs/stdouterr.DESeq2.", runConfig$comparison[rowId],".log", sep = ""),
+#     stderr = paste("logs/stdouterr.DESeq2.", runConfig$comparison[rowId],".log", sep = "")
+#   )
+# 
+# }
 
 ###########################################################################
 ####################################
 ### DESeq2 Functional enrichment ###
 ####################################
 
-file_RNAseq_info <- here::here("data", "DESeq2_DEG_info.txt")
-diffDataPath <- here::here("analysis", "02_DESeq2_diff")
-script_deseq2_GO <- here::here("scripts", "03_RNAseq_functional_enrichment.R")
+file_productionData <- here::here("data", "reference_data", "production_data.summary.tab")
+diffDataPath <- here::here("analysis", "06_polII_diff")
+script_deseq2_GO <- here::here("scripts", "a_explore_polII", "a09_polII_DEGs_functional_enrichment.R")
+file_RNAseq_info <- here::here("data", "reference_data", "polII_DESeq2_DEG_info.txt")
 
-runConfig <- suppressMessages(readr::read_tsv(file_RNAseq_info))
 
-i <- 1
+productionData <- suppressMessages(readr::read_tsv(file = file_productionData)) %>% 
+  dplyr::filter(has_polII_ChIP == "has_data")
 
-for (i in 1:nrow(runConfig)) {
+
+rowId <- 1
+
+for (rowId in 1:nrow(productionData)) {
   
   system2(
     command = "Rscript",
     # args = c(script_deseq2_GO, "--help"),
-    args = c(script_deseq2_GO, "--config", file_RNAseq_info, "--deg", runConfig$comparison[i]),
-    stdout = paste("stdouterr.functional_enrichment.", runConfig$comparison[i],".log", sep = ""),
-    stderr = paste("stdouterr.functional_enrichment.", runConfig$comparison[i],".log", sep = "")
+    args = c(script_deseq2_GO, "--config", file_RNAseq_info, "--deg", productionData$degId[rowId]),
+    stdout = paste("logs/stdouterr.functional_enrichment.", productionData$degId[rowId],".log", sep = ""),
+    stderr = paste("logs/stdouterr.functional_enrichment.", productionData$degId[rowId],".log", sep = "")
   )
   
+  print(productionData$degId[rowId])
 }
+
 
 ###########################################################################
 
+# ## run DESeq2 and RankComp2 comparison script for all OE/WT polII comparisons
+# file_RNAseq_info <- here::here("data", "reference_data", "polII_DESeq2_DEG_info.txt")
+# runConfig <- suppressMessages(readr::read_tsv(file_RNAseq_info))
+# script <- here::here("scripts", "21_DESeq2_RankComp.comparison.R")
+# 
+# rowId <- 47
+# 
+# for (rowId in 1:nrow(runConfig)) {
+#   
+#   cat("Running", basename(script), "for", runConfig$comparison[rowId], "\n")
+#   
+#   system2(
+#     command = "Rscript",
+#     # args = c(script, "--help"),
+#     args = c(script, "--deg", runConfig$comparison[rowId]),
+#     stdout = paste("logs/stdouterr.DESeq2_RankComp2_comp.", runConfig$comparison[rowId],".log", sep = ""),
+#     stderr = paste("logs/stdouterr.DESeq2_RankComp2_comp.", runConfig$comparison[rowId],".log", sep = "")
+#   )
+# 
+# }
 
-
-## copy results for sharing
-file_RNAseq_info <- here::here("data", "DESeq2_DEG_info.txt")
-diffDataPath <- here::here("analysis", "02_DESeq2_diff")
-
-runConfig <- suppressMessages(readr::read_tsv(file_RNAseq_info))
-
-tempStore <- here::here("analysis", "temp_store")
-dir.create(tempStore)
-i <- 1
-
-for (i in 1:nrow(runConfig)) {
-  
-  newDir <- paste(tempStore, "/", runConfig$comparison[i], sep = "")
-  dir.create(newDir)
-  
-  file_deseq2Res <- paste(diffDataPath, "/", runConfig$comparison[i], "/", runConfig$comparison[i], ".DEG_all.xlsx", sep = "")
-  file_enrich <- paste(diffDataPath, "/", runConfig$comparison[i], "/", runConfig$comparison[i], ".enrichment.xlsx", sep = "")
-  file_plots <- paste(diffDataPath, "/", runConfig$comparison[i], "/", runConfig$comparison[i], ".summary_plots.pdf", sep = "")
-
-  file.copy(from = file_deseq2Res, to = newDir)
-  file.copy(from = file_enrich, to = newDir)
-  file.copy(from = file_plots, to = newDir)
-  
-}
-
+###########################################################################
 
 
 
